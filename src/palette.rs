c6b4c5be85dc45b62a056980999934920f902ff6
Aramis Razzaghipour <aramisnoah@gmail.com>
2021-06-17 22:39:43 +1000

    Homogenise colours

--- src/palette.rs
+++ src/palette.rs
@@ -9,35 +9,35 @@ impl Palette {
     }
 
     pub(crate) fn red(&self) -> Oklch {
-        oklch(0.6, 0.12, 15.0)
+        oklch(0.65, 0.1, 15.0)
     }
 
     pub(crate) fn orange(&self) -> Oklch {
-        oklch(0.7, 0.1, 40.0)
+        oklch(0.7, 0.07, 40.0)
     }
 
     pub(crate) fn brown(&self) -> Oklch {
-        oklch(0.6, 0.07, 40.0)
+        oklch(0.6, 0.04, 40.0)
     }
 
     pub(crate) fn yellow(&self) -> Oklch {
-        oklch(0.85, 0.09, 85.0)
+        oklch(0.85, 0.07, 85.0)
     }
 
     pub(crate) fn green(&self) -> Oklch {
-        oklch(0.75, 0.07, 130.0)
+        oklch(0.7, 0.07, 130.0)
     }
 
     pub(crate) fn cyan(&self) -> Oklch {
-        oklch(0.77, 0.03, 195.0)
+        oklch(0.7, 0.04, 195.0)
     }
 
     pub(crate) fn blue(&self) -> Oklch {
-        oklch(0.7, 0.03, 250.0)
+        oklch(0.7, 0.04, 250.0)
     }
 
     pub(crate) fn purple(&self) -> Oklch {
-        oklch(0.7, 0.06, 335.0)
+        oklch(0.7, 0.07, 335.0)
     }
 }
 
